f7c7ffe85cd161e48387483b798c5c5017ed4610
Satoru Nishizawa <satoru.nh@gmail.com>
2021-12-03 14:24:35 +0900

    basic crud, main, schemas are ready

--- sql_app/models.py
+++ sql_app/models.py
@@ -1,14 +1,14 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime, Time
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.functions import current_timestamp
 from datetime import datetime, time
+from .database import Base
 from enum import Enum
-from database import Base
-
 class Priority(Enum):
-    A = 1
-    B = 2
-    C = 3
-    D = 4
+    a = 1
+    b = 2
+    c = 3
+    d = 4
 
 class Category(Enum):
     daily = 'daily'
@@ -16,7 +16,6 @@ class Category(Enum):
     monthly = 'monthly'
     yearly = 'yearly'
     decade = 'decade'
-
 class User(Base):
     __tablename__="users"
 
@@ -33,14 +32,15 @@ class Task(Base):
     __tablename__ = "tasks"
     id = Column('id', Integer, primary_key=True, index=True)
     user_id = Column('user_id', Integer, ForeignKey("users.id"))
-    priority = Column('priority', Priority)
-    title = Column('title', String, index=True)
+    priority = Column('priority', Integer)
+    category = Column('category', String)
+    title = Column('title', String)
     assigned_user = relationship("User", back_populates="tasks")
     items = relationship("Item", back_populates="task_title")
-    deadline = Column('deadline', datetime, default=None, nullable=False)
-    created = Column('created', datetime, default=datetime.now(), nullable=False, server_default=datetime.current_timestamp())
-    estimated_time = Column('estimated_time', time, default=time.now(), nullable=True)
-    spent_time = Column('spent_time', time, default=time.now(), nullable=True)
+    deadline = Column('deadline', DateTime, default=None, nullable=False)
+    created = Column('created', DateTime, default=datetime.now(), nullable=False, server_default=current_timestamp())
+    estimated_time = Column('estimated_time', Integer, default=0, nullable=True)
+    spent_time = Column('spent_time', Integer, default=0, nullable=True)
 
     done = Column('done', Boolean, default=False, nullable=False)
  
--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,11 +1,15 @@
 from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime, time
-from sql_app.models import Priority
+from enum import Enum
+from sql_app.models import Priority, Category
 
 class ItemBase(BaseModel):
     title: Optional[str] = None
     description: Optional[str] = None
+
+class ItemCreate(ItemBase):
+    pass
 
 class Item(BaseModel):
     id: int
@@ -13,7 +17,8 @@ class Item(BaseModel):
     task_id: int
 class TaskBase(BaseModel):
     title: str
-    priority: Priority
+    priority: Enum
+    category: Enum
     items: Optional[List[Item]] = []
     deadline: datetime
     created: datetime
@@ -21,6 +26,8 @@ class TaskBase(BaseModel):
     spent_time: Optional[time] = None
     class Config:
         orm_mode = True
+class TaskCreate(TaskBase):
+    pass
 class Task(TaskBase):
     id: int
     user_id: int
